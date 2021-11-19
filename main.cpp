544d91589c00518fcf3c27298f4cc0e9b2a0f4c1
schwarzam <gustavo.b.schwarz@gmail.com>
2021-11-18 21:03:54 -0300

    steam lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "steam/steam_api.h"
+
+S_API bool S_CALLTYPE SteamAPI_Init();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+    std::cout << SteamAPI_Init() << std::endl;
 }
