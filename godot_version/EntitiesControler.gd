6b8daf989339532d4d54df80ce0c0b1baf94dbe8
schwarzam <gustavo.b.schwarz@gmail.com>
2022-12-08 12:33:46 -0300

    started working on menu

--- godot_version/EntitiesControler.gd
+++ godot_version/EntitiesControler.gd
@@ -1,10 +1,14 @@
 extends Node
 
+signal ended_game
+
+var GAME_RUNNING = false
 var entities = {}
 var new_entity = {
 	life = 20
 }
 var user
+
 	
 func _ready():
 	
@@ -15,6 +19,11 @@ func _process(delta):
 	for i in entities:
 		pass
 		#print(entities[i])
+	
+	if GAME_RUNNING and entities.size() <= 0:
+		end_game()
+	
+	
 	
 func _register_user(user):
 	self.user = user
@@ -28,10 +37,13 @@ func _on_damage(id):
 	if entities[id].life <= 0:
 		var node = instance_from_id(id)
 		node.on_death()
+		entities.erase(id)
 		
 
-func print_tei():
-	pass
+func end_game():
+	GAME_RUNNING = false
+	EntitiesControler.emit_signal("ended_game")
+	get_tree().change_scene("res://Game/Menu.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
---
+++ godot_version/Game/Game.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	EntitiesControler.connect("ended_game", self, "_on_ended_game")
+	pass # Replace with function body.
+	
+func _on_ended_game():
+	print("Ended game")
+	queue_free()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
