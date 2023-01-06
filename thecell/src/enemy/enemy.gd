bd9ac72c4b82868d0b0eeb895db17ecad77ac795
schwarzam <gustavo.b.schwarz@gmail.com>
2023-01-06 11:09:19 -0300

    zombie attack working

--- thecell/src/enemy/enemy.gd
+++ thecell/src/enemy/enemy.gd
@@ -9,6 +9,8 @@ var velocity = Vector3.DOWN
 var threshold = .1
 # Called when the node enters the scene tree for the first time.
 
+var attacking = false
+
 onready var target = null
 onready var nav = get_tree().get_root().get_node("Game/Map/Navigation")
 
@@ -29,8 +31,10 @@ func move_to_target():
 	else:
 		var direction = (path[cur_path_idx] - global_transform.origin)
 		velocity = direction.normalized()
-		move_and_slide(velocity, Vector3.UP)
-		$Persona/AnimationPlayer.play("walking")
+		
+		if not attacking:
+			move_and_slide(velocity, Vector3.UP)
+			$Persona/AnimationPlayer.play("walking")
 	
 	var look_direction = Vector2(velocity.z, velocity.x)
 	$Persona.rotation.y =  lerp($Persona.rotation.y, look_direction.angle(), 0.5)
@@ -51,3 +55,10 @@ func _on_Timer_timeout():
 	else:
 		$Persona/AnimationPlayer.play("idle")
 
+func _on_attack_animation_finished():
+	attacking = false
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("Players"):
+		attacking = true
+		$Persona/AnimationPlayer.play("punch")
